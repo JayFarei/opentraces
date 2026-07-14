7e4180c4fe5d7949b486e39c9bd6dc95447bae52
jayfarei <fareiunastrage@gmail.com>
2026-07-14 04:34:58 +0100

    fix(arena): inspect sidecar liveness without signalling

--- src/opentraces/core/arena/emulate/huggingface/runtime.py
+++ src/opentraces/core/arena/emulate/huggingface/runtime.py
@@ -641,7 +641,9 @@ def start_huggingface_emulator(
             "sh",
             "-c",
             "set -eu; pid=$(cat /tmp/opentraces-hf-emulator.pid); "
-            f'test "$pid" = "{pid}"; kill -0 "$pid"; '
+            f'test "$pid" = "{pid}"; test -r "/proc/$pid/stat"; '
+            'state=$(sed -n "s/^.*) \\([A-Z]\\) .*$/\\1/p" "/proc/$pid/stat"); '
+            'test -n "$state"; test "$state" != "Z"; '
             "owner_uid=$(awk '/^Uid:/{print $2}' \"/proc/$pid/status\"); "
             'sidecar_uid=$(id -u opentraces-hf); test "$owner_uid" = "$sidecar_uid"; '
             'executable=$(readlink -f "/proc/$pid/exe"); '
